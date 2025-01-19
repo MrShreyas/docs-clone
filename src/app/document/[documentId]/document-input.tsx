aceeaacc5a2e6a990285cc37ce35e78efe40ab96
Shreyas Katalkar <katalkarshreyas@gmail.com>
2025-01-19 18:13:11 +0530

    minor bug

--- src/app/document/[documentId]/document-input.tsx
+++ src/app/document/[documentId]/document-input.tsx
@@ -3,7 +3,6 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import React, { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { Form } from "react-hook-form";
 import { useDebounce } from "@/hooks/use-debounce";
 import { toast } from "sonner";
 import { useStatus } from "@liveblocks/react";
